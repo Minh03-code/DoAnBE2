0eb2d8732cfb0e2ffc3f87cca75510806a077523
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-05-22 21:36:10 +0700

    Update css

--- DoAnBE2/resources/views/CustomerInterface/change-password.blade.php
+++ DoAnBE2/resources/views/CustomerInterface/change-password.blade.php
@@ -1,27 +1,29 @@
 @extends('CustomerInterface/main')
 
 @section('content')
-    <div class="container">
-        <p class="p-addpr">Đổi mật khẩu</p>
-        <div class="content-of-add-product">
-            <form action="#" method="POST">
-                <input type="hidden" name="account-id"> {{-- Phải có category để biết đang sửa của tài khoản nào --}}
-                <div class="mb-3">
-                    <label for="old-password" class="form-label">Mật khẩu</label>
-                    <input type="password" class="form-control" id="edit-password" name="edit-password">
-                </div>
+    <div class="form-margin-m">
+        <div class="container">
+            <p class="title-form-n">Đổi mật khẩu</p>
+            <div class="content-of-add-product">
+                <form action="#" method="POST">
+                    <input type="hidden" name="account-id"> {{-- Phải có category để biết đang sửa của tài khoản nào --}}
+                    <div class="mb-3">
+                        <label for="old-password" class="form-label">Mật khẩu</label>
+                        <input type="password" class="form-control" id="edit-password" name="edit-password">
+                    </div>
 
-                <div class="mb-3">
-                    <label for="new-password" class="form-label">Mật khẩu mới</label>
-                    <input type="password" class="form-control" id="new-password" name="new-password">
-                </div>
-                <div class="mb-3">
-                    <label for="confirm-password" class="form-label">Xác nhận mật khẩu mới</label>
-                    <input type="password" class="form-control" id="confirm-password" name="confirm-password">
-                </div>
-                <button type="submit" class="btn btn-primary">Đổi mật khẩu</button>
-            </form>
+                    <div class="mb-3">
+                        <label for="new-password" class="form-label">Mật khẩu mới</label>
+                        <input type="password" class="form-control" id="new-password" name="new-password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="confirm-password" class="form-label">Xác nhận mật khẩu mới</label>
+                        <input type="password" class="form-control" id="confirm-password" name="confirm-password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Đổi mật khẩu</button>
+                </form>
+            </div>
+
         </div>
-
     </div>
 @endsection
--- DoAnBE2/resources/views/CustomerInterface/form-profile.blade.php
+++ DoAnBE2/resources/views/CustomerInterface/form-profile.blade.php
@@ -1,31 +1,33 @@
 @extends('CustomerInterface/main')
 
 @section('content')
-    <div class="container">
-        <p class="p-addpr">Sửa thông tin</p>
-        <div class="content-of-add-product">
-            <form action="#" method="POST">
-                <input type="hidden" name="account-id"> {{-- Phải có category để biết đang sửa của tài khoản nào --}}
-                <div class="mb-3">
-                    <label for="edit-name-customer" class="form-label">Tên người dùng</label>
-                    <input type="text" class="form-control" id="edit-name-customer" name="edit-name-customer">
-                </div>
-                <div class="mb-3">
-                    <label for="address" class="form-label">Địa chỉ</label>
-                    <input type="text" class="form-control" id="address" name="address">
-                </div>
-                <div class="mb-3">
-                    <label for="edit-email" class="form-label">Email</label>
-                    <input type="text" class="form-control" id="edit-email" name="edit-email">
-                </div>
-                <div class="mb-3">
-                    <label for="edit-phone" class="form-label">Số điện thoại</label>
-                    <input type="text" class="form-control" id="edit-phone" name="edit-phone">
-                </div>
+    <div class="form-margin-m">
+        <div class="container">
+            <p class="title-form-n">Sửa thông tin</p>
+            <div class="content-of-add-product">
+                <form action="#" method="POST">
+                    <input type="hidden" name="account-id"> {{-- Phải có category để biết đang sửa của tài khoản nào --}}
+                    <div class="mb-3">
+                        <label for="edit-name-customer" class="form-label">Tên người dùng</label>
+                        <input type="text" class="form-control" id="edit-name-customer" name="edit-name-customer">
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Địa chỉ</label>
+                        <input type="text" class="form-control" id="address" name="address">
+                    </div>
+                    <div class="mb-3">
+                        <label for="edit-email" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="edit-email" name="edit-email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="edit-phone" class="form-label">Số điện thoại</label>
+                        <input type="text" class="form-control" id="edit-phone" name="edit-phone">
+                    </div>
 
-                <button type="submit" class="btn btn-primary">Chấp nhận thay đổi</button>
-            </form>
+                    <button type="submit" class="btn btn-primary">Chấp nhận thay đổi</button>
+                </form>
+            </div>
+
         </div>
-
     </div>
 @endsection
