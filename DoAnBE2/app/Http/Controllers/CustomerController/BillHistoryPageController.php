5da6a799c979861e9936fa58a1741c32c06e9eda
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-05-26 14:15:00 +0700

    Create page controller

---
+++ DoAnBE2/app/Http/Controllers/CustomerController/BillHistoryPageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\CustomerController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BillHistoryPageController extends Controller
+{
+    //
+}
