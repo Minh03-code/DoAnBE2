f29b80722ec5e138785b1d1813604bae9415bd87
linh1006 <21211tt4494@mail.tdc.edu.vn>
2023-05-26 16:17:11 +0700

    Create function 11 v1

--- DoAnBE2/app/Models/Product.php
+++ DoAnBE2/app/Models/Product.php
@@ -9,4 +9,11 @@ class Product extends Model
 {
     use HasFactory;
     protected $table = 'products';
+    //Start func 11 v1.0
+    public static function getProductByID($id)
+    {
+        $sanpham = Product::find($id);
+        return $sanpham;
+    }
+    //End func 11 v1.0
 }
