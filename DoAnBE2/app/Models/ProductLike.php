fd93737fc9aad7e23f158acd33f6bac38bb0a73d
dung <21211tt4157@mail.dc.edu.vn>
2023-05-26 18:36:37 +0700

    edit func account like

--- DoAnBE2/app/Models/ProductLike.php
+++ DoAnBE2/app/Models/ProductLike.php
@@ -9,4 +9,8 @@ class ProductLike extends Model
 {
     use HasFactory;
     protected $table = 'product_likes';
+
+    public static function countProductsLikedByAccountID($id_account){
+        return count(self::where('account_id',$id_account)->get());
+    }
 }
