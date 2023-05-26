1f7bec513b464c8b55d4f330f8e0db7a99d85594
nguyenthihong190402 <105511378+nguyenthihong190402@users.noreply.github.com>
2023-05-26 16:11:35 +0700

    2023/26/05 Create function getItemsInCartTableByAccountID

--- DoAnBE2/app/Models/Cart.php
+++ DoAnBE2/app/Models/Cart.php
@@ -9,4 +9,12 @@ class Cart extends Model
 {
     use HasFactory;
     protected $table = 'carts';
+
+    //Start function 12
+    public static function getItemsInCartTableByAccountID($account_id)
+    {
+        $result = self::where('account_id', '=', $account_id)->get();
+        return $result;
+    }
+    //End function 12
 }
