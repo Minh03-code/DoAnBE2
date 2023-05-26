effb4c3e6307c3d35941633f40b35a0ca04e3b9d
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-05-26 16:03:15 +0700

    create function getPaginationBillsByAccountID

--- DoAnBE2/app/Models/Bill.php
+++ DoAnBE2/app/Models/Bill.php
@@ -9,4 +9,18 @@ class Bill extends Model
 {
     use HasFactory;
     protected $table = 'bills';
+
+    public function getProductsOfBill(){
+        $this->setAttribute("products_of_bill", $this->hasMany(BillProduct::class, "bill_id", "id")->get());
+    }
+
+    public static function getPaginationBillsByAccountID($account_id, $pagination) {
+        $result = self::where('account_id', $account_id)->paginate($pagination);
+        if ($result){
+            foreach ($result as $item) {
+                $item->getProductsOfBill();
+            }
+        }
+        return $result;
+    }
 }
