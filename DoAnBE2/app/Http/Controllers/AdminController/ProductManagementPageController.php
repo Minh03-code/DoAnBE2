dc4a9e33caaff9e19f7d065a160e1e427b4aff7e
gianghiem0310 <jangiim93nt@gmail.com>
2023-06-03 07:58:23 +0700

    edit func 9

--- DoAnBE2/app/Http/Controllers/AdminController/ProductManagementPageController.php
+++ DoAnBE2/app/Http/Controllers/AdminController/ProductManagementPageController.php
@@ -24,7 +24,13 @@ class ProductManagementPageController extends MainAdminController
     public function changeStatusOfProductByProduct(Request $request)
     {
         $product = Product::find($request->id);
+        if($product->status==0)
+        {
         $product->status = 1;
+        }
+        else{
+            $product->status = 0;
+        }
         $product->update();
         return redirect('admin/product-management');
         
