ae35af4f6a1e14fcd520c52e56f82037d972e2e6
gianghiem0310 <jangiim93nt@gmail.com>
2023-06-03 08:36:53 +0700

    edit image in shop grid

--- DoAnBE2/resources/views/CustomerInterface/shop-grid.blade.php
+++ DoAnBE2/resources/views/CustomerInterface/shop-grid.blade.php
@@ -44,7 +44,7 @@ use App\Models\ProductLike;
                          @foreach ($listProduct as $listP)
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{asset('img/'.$product->image)}}">
+                                <div class="product__item__pic set-bg" data-setbg="{{asset('img/'.$listP->image)}}">
                                     <ul class="product__item__pic__hover">
                                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                         <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
